6c0e14bf1e71f12b08f7e17b763237862a3dc157
mikivan1980 <mikivan1980@mail.ru>
2017-09-30 20:02:25 +0300

    ohif gateway studylist is good

--- src/main/java/com/mikivan/OHIFGateway/GateController.java
+++ src/main/java/com/mikivan/OHIFGateway/GateController.java
@@ -1,8 +1,8 @@
 package com.mikivan.OHIFGateway;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -32,17 +32,13 @@ public class GateController {
     public ResponseEntity<String> getStudylist(
             @RequestParam Map<String, String> queryMap ) {
 
-        //log.info("/mikivan/studies?<RequestParam>");
 
-        //выводим параметры запроса пришедшие с meteor
-        //log.info("RequestParam: " + queryMap.toString() );
+        ArrayList<String> opts = new ArrayList<String>();
 
+        opts.add("-b"); opts.add("IVAN@192.168.0.74:4006");
+        opts.add("-c"); opts.add("PACS01@192.168.0.35:4006");
+        opts.add("-L"); opts.add("STUDY");
 
-        ArrayList<String> test = new ArrayList<String>();
-
-        test.add("-b"); test.add("IVAN@192.168.0.74:4006");
-        test.add("-c"); test.add("PACS01@192.168.0.35:4006");
-        test.add("-L"); test.add("STUDY");
 
 //onCoreViewer/Packages/ohif-study-list/server/services/qido/studies.js:20
 //PatientName
@@ -53,115 +49,71 @@ public class GateController {
 //limit         игнорируем (а многоли включать данных в ответ)
 //includefield  игнорируем (какие еще поля  вернуть, мы вернем необходимый минимум)
 //StudyDate
-
         for( Map.Entry<String, String> entry : queryMap.entrySet() ){
 
             if(  (!entry.getKey().equals("limit")) &&
                     (!entry.getKey().equals("includefield"))
                     ) {
 
-                test.add("-m");test.add(entry.getKey() + "=" + entry.getValue());
+                opts.add("-m");opts.add(entry.getKey() + "=" + entry.getValue());
 
                 System.out.println("-m " + entry.getKey() + "=" + entry.getValue());
             }
         }
 
 //http://192.168.0.74:8080/mikivan/studies?PatientName=Mikh*&ModalitiesInStudy=CT&includefield=00081030%2C00080060
+        //параметры взяты из метеора
+        String[] r = {"0020000D", "00080020", "00080030", "00080050", "00080090", "00100010", "00100020", "00100030",
+                      "00100040", "00200010", "00201206", "00201208", "00081030", "00080060", "00080061"};
 
-        test.add("-r");test.add("00080020");
-        test.add("-r");test.add("00080030");
-        test.add("-r");test.add("00080060");
-        test.add("-r");test.add("00080061");
-//         test.add("-r");test.add("00080090");
-//         test.add("-r");test.add("00081030");
-        test.add("-r");test.add("00081190");
-        test.add("-r");test.add("00100010");
-        test.add("-r");test.add("00100020");
-        test.add("-r");test.add("00100030");
-        test.add("-r");test.add("00100040");
-        test.add("-r");test.add("0020000D");
-        test.add("-r");test.add("00200010");
-        test.add("-r");test.add("00201206");
-        test.add("-r");test.add("00201208");
+        for(String teg:r) {
+            opts.add("-r");opts.add(teg);
+        }
 
-        test.add("--out-cat");
-        //test.add("--xsl");test.add("xslt/oncore-json_compact.xsl");
-        test.add("--xsl");test.add("xslt/mikivan-studies.xsl");
-        test.add("-K");
-        test.add("-I");
+        opts.add("--out-cat");
+
+        //либо либо либо
+        //opts.add("-X");
+        //opts.add("--xsl");opts.add("xslt/oncore-json_compact.xsl");
+        opts.add("--xsl");opts.add("xslt/mikivan-studies.xsl");
+
+        opts.add("-K");
+        opts.add("-I");
 
 
-        String val2 =  "";
+        String toViewStudyList =  "";
 
-        //[injections of mikivan][0004]
         try {
 
-            String[] args = new String[test.size()];
+            String[] args = new String[opts.size()];
 
-            test.toArray(args);
-
-/*             String[] args = {
-                     "-b", "IVAN@192.168.0.74:4006",
-                     "-c", "PACS01@192.168.0.35:4006",
-                     "-L", "STUDY",
-                     "-m", "StudyDate=20120101-20161231",
-//                     "-m", "ModalitiesInStudy=CT",
-                     "-m", "PatientSex=M",
-//                     "-r", "00080005",
-                     "-r", "00080020",
-                     "-r", "00080030",
-//                     "-r", "00080050",
-                     "-r", "00080060",
-                     "-r", "00080061",
-//                     "-r", "00080090",
-//                     "-r", "00081030",
-//                     "-r", "00081190",
-                     "-r", "00100010",
-                     "-r", "00100020",
-                     "-r", "00100030",
-                     "-r", "00100040",
-                     "-r", "0020000D",
-                     "-r", "00200010",
-                     "-r", "00201206",
-                     "-r", "00201208",
-                     "--out-cat",
-                     "--xsl", "xslt/oncore-json_compact.xsl",//"-X",//
-                     "-K",
-                     "-I"
-             };*/
-
+            opts.toArray(args);
 
             FindSCU main = new FindSCU(args);
 
             String jsonOutput = main.doFind();
 
-
-            //вывод doFind();
             if( jsonOutput == null ){
-                System.out.println("xmlOutput == null");
+                System.out.println("jsonOutput is null");
             }
             else {
-                System.out.println(jsonOutput);
+                System.out.println("jsonOutput is created");
 
-                val2 =  val2 + "[" + jsonOutput + "{}]";
+                toViewStudyList =  toViewStudyList + "[" + jsonOutput + "{}]";
             }
-
-
         } catch (Exception e) {
             System.err.println("findscu: " + e.getMessage());
             e.printStackTrace();
             System.exit(2);
         }
-        //[end][0004]
 
 
-
-        //готовим ответ на запрос /mikivan/studylist
+        //ответ на запрос /mikivan/studylist
         HttpHeaders hdrs = new HttpHeaders();
         final MediaType mediaType = MediaType.APPLICATION_JSON;
         hdrs.setContentType(mediaType);
 
-        final ResponseEntity<String> rsp = new ResponseEntity<String>(val2, hdrs, HttpStatus.OK);
+        final ResponseEntity<String> rsp = new ResponseEntity<String>(toViewStudyList, hdrs, HttpStatus.OK);
 
         return rsp;
     }
@@ -177,7 +129,7 @@ public class GateController {
         //http://192.168.0.74:8080/mikivan/studies/1.2.840.113704.1.111.4156.1367430813.2/metadata
 
         //log.info("/mikivan/studies/{studyUID}/metadata");
-        System.out.println("/mikivan/studies/{studyUID}/metadata = " + "/mikivan/studies/" + studyUID + "/metadata = ");
+        System.out.println("/mikivan/studies/{studyUID}/metadata = " + "/mikivan/studies/" + studyUID + "/metadata");
 
         ArrayList<String> test = new ArrayList<String>();
 
--- xslt/mikivan-studies.xsl
+++ xslt/mikivan-studies.xsl
@@ -20,24 +20,56 @@
 
         <xsl:choose>
 
-            <xsl:when test="@tag='0020000D'">
-                <xsl:text>"0020000D": {Ok},</xsl:text>
+            <xsl:when test="@tag='0020000D'
+                         or @tag='00080020'
+                         or @tag='00080030'
+                         or @tag='00080050'
+                         or @tag='00080090'
+                         or @tag='00100010'
+                         or @tag='00100020'
+                         or @tag='00100030'
+                         or @tag='00100040'
+                         or @tag='00200010'
+                         or @tag='00201206'
+                         or @tag='00201208'
+                         or @tag='00081030'
+                         or @tag='00080060'
+                         or @tag='00080061'">
+
+                <xsl:if test="position()>1">,</xsl:if>
+
+                <xsl:text>"</xsl:text>
+                <xsl:value-of select="@tag"/>
+                <xsl:text>"</xsl:text>
+
+                <xsl:text>:{"vr":"</xsl:text>
+                <xsl:value-of select="@vr"/>
+                <xsl:text>",</xsl:text>
+
+
+                <xsl:text>"Value":["</xsl:text>
+
+                <xsl:apply-templates mode="mikivan" select="Value" />
+                <xsl:apply-templates mode="mikivan" select="PersonName" />
+
+                <xsl:text>"]}</xsl:text>
+
             </xsl:when>
 
-            <xsl:when test="@tag='00080020'">
-                <xsl:text>"00080020": {Ok},</xsl:text>
-            </xsl:when>
-
-            <xsl:when test="@tag='00080030'">
-                <xsl:text>"00080030": {Ok},</xsl:text>
-            </xsl:when>
-
-            <xsl:otherwise>"other":{},</xsl:otherwise>
+            <xsl:otherwise></xsl:otherwise>
 
         </xsl:choose>
+
 
     </xsl:template>
 
 
+    <xsl:template match="Value" mode="mikivan">
+         <xsl:value-of select="text()"/>
+    </xsl:template>
+
+    <xsl:template match="PersonName" mode="mikivan">
+        <xsl:apply-templates select="*"/>
+    </xsl:template>
 
 </xsl:stylesheet>
